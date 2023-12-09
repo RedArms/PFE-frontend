2f86bbcc5ee8eb5db0762890378b20ee541b7640
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-12-09 12:51:45 +0100

    Untrack .env file

--- src/components/CrecheLine/CrecheLine.tsx
+++ src/components/CrecheLine/CrecheLine.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Text,TouchableWithoutFeedback } from 'react-native';
+import styles from './CrecheLineStyle';
 
 interface Props {
-    creche: string;
-    article: {
-        name: string;
-        quantity: number;
-    }[];
+  creche: string;
+  article: {
+    name: string;
+    quantity: number;
+  }[];
 }
 
-const CrecheLine: React.FC<Props> = ({ creche }) => {
-    return (
-        <View>
-            <Text>{creche}</Text>
+const CrecheLine: React.FC<Props> = ({ creche, article }) => {
+    const [isArticleVisible, setisArticleVisible] = useState(false);
+    const toggleArticleVisible = () => {
+      setisArticleVisible(!isArticleVisible);
+    };
+
+  return (
+    
+    <ScrollView>
+    <View style={styles.toursChooseLine}>
+      <TouchableWithoutFeedback onPress={toggleArticleVisible}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.text}>{creche}</Text>
         </View>
-    );
+      </TouchableWithoutFeedback>
+
+      {isArticleVisible && (
+        <ScrollView style={{ ...styles.crecheContainer}}>
+          {article.map((articleLine, index) => (
+            <Text style={styles.crecheLine} key={index}>
+              {articleLine.name} : {articleLine.quantity}
+            </Text>
+          ))}
+        </ScrollView>
+      )}
+    </View>
+  </ScrollView>
+  );
 };
 
 export default CrecheLine;
