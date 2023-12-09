e16e71808e84c0500603753258c9497f8f94899b
Brahim <brahim.abid@student.vinci.be>
2023-12-09 23:31:08 +0100

    Add random word to item labels

--- src/screens/admin/ItemManagementScreen.tsx
+++ src/screens/admin/ItemManagementScreen.tsx
@@ -13,11 +13,16 @@ const ItemManagementScreen: React.FC = () => {
   const itemsList: Item[] = [
   ];
 
+  const getRandomWord = () => {
+    const words = ["couches", "biberons", "lits", "jouets", "vêtements", "poussettes", "serviettes", "cuisinettes"];
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  };
  // add 25 items to the list for the example
   for (let i = 1; i < 25; i++) {
     const item: Item = {
       item_id: i,
-      label: `Article ${i}`,
+      label: `*${i}* ${getRandomWord()} `,
       size: i % 2 == 0 ? "M" : i % 3 == 0 ? "L" : undefined,
     };
     itemsList.push(item);
