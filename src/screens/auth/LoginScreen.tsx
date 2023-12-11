97b584c5e101edbead3ce07fa8bc276b936522e7
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-11 02:38:03 +0100

    Fix import casing in AddItemComponent and Item components

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useContext } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
-
 import { UserContext } from "../../contexts/UserContext";
 import { User } from "../../models/user";
 import Logo from "../../components/Logo/Logo";
 import { login as loginApi } from "../../services/authService";
-
 
 const LoginScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
   const [email, setEmail] = useState("");
