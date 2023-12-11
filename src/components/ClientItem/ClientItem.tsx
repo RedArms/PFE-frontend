c493299f088282d1a6837bf3a32909a4a373077d
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-11 02:22:43 +0100

    Refactor ClientItem component to include navigation

--- src/components/ClientItem/ClientItem.tsx
+++ src/components/ClientItem/ClientItem.tsx
@@ -1,32 +1,41 @@
-// ClientItem.tsx
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { Client } from '../../models/Client';
 
-const ClientItem = ({ name }: { name: string }) => {
-    const styles = StyleSheet.create({
-        clientItem: {
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: '#007AFF',
-            borderRadius: 10,
-            marginVertical: 10,
-            marginHorizontal: 20,
-            padding: 20,
-        },
-        clientText: {
-            color: 'white',
-            fontSize: 18,
-        }
-    });
+const ClientItem = ({ client }: { client: Client }) => {
+	const navigation = useNavigation();
 
-    return (
-        <View style={styles.clientItem}>
-            <Text style={styles.clientText}>{name}</Text>
-            <Ionicons name="md-settings" size={24} color="black" />
-        </View>
-    )
+	const navigateToClientDetails = () => {
+		navigation.navigate('ClientDetailsScreen', { client });
+	};
+
+	return (
+		<TouchableOpacity onPress={navigateToClientDetails}>
+			<View style={styles.clientItem}>
+				<Text style={styles.clientText}>{client.name}</Text>
+				<Ionicons name="settings" size={24} color="black" />
+			</View>
+		</TouchableOpacity>
+	)
 };
+
+const styles = StyleSheet.create({
+	clientItem: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		backgroundColor: '#007bff',
+		borderRadius: 10,
+		marginVertical: 10,
+		marginHorizontal: 20,
+		padding: 20,
+	},
+	clientText: {
+		color: 'white',
+		fontSize: 18,
+	}
+});
 
 export default ClientItem;
