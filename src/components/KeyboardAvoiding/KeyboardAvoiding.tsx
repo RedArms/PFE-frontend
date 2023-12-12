d44b46682682f71c8a4edd137966c4c08b276183
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-12 02:42:48 +0100

    Refactor KeyboardAvoidingComponent to use arrow function syntax

--- src/components/KeyboardAvoiding/KeyboardAvoiding.tsx
+++ src/components/KeyboardAvoiding/KeyboardAvoiding.tsx
@@ -3,14 +3,14 @@ import { Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } fr
 // this component can be used in any screen and can be customized
 // this component is used to dismiss keyboard when user click outside of input
 const KeyboardAvoidingComponent: React.FC<{ children: React.ReactNode }>  = ({children}) => {
-    return (
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          {children}
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    );
-  };
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        {children}
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
 
-  export default KeyboardAvoidingComponent;
+export default KeyboardAvoidingComponent;
