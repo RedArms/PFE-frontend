dd343585e3864bb28faf4b52c9bd36e947c19c84
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-12 02:43:24 +0100

    Refactor ClientsScreen component

--- src/screens/clients/ClientsScreen.tsx
+++ src/screens/clients/ClientsScreen.tsx
@@ -3,12 +3,11 @@ import { View } from 'react-native';
 import ClientList from '../../components/ClientList/ClientList';
 
 const ClientsScreen: React.FC = () => {
-
-    return (
-        <View style={{flex: 1, backgroundColor: '#F5F5F5'}}>
-            <ClientList />
-        </View>
-    );
+  return (
+    <View style={{flex: 1, backgroundColor: '#F5F5F5'}}>
+      <ClientList />
+    </View>
+  );
 };
 
 export default ClientsScreen;
