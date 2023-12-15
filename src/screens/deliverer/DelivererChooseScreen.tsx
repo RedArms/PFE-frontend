05f268cdf9c5c9152766e98da3a3ae9273e34bd6
Younes Benbouchta <younes.benbouchta@student.vinci.be>
2023-12-15 02:24:21 +0100

    Refactor DelivererChooseScreen component

--- src/screens/deliverer/DelivererChooseScreen.tsx
+++ src/screens/deliverer/DelivererChooseScreen.tsx
@@ -2,10 +2,9 @@ import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { getMonthString } from "../../utils/month";
 import ToursChoose from "../../components/toursChoose/toursChoose";
-import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
-import { TourContext } from "../../contexts/TourContext";
+
 // cette page s'affiche quand le livreur n'a pas de tournée en cours, il a donc le choix entre les tournées du jour
-const DelivererChooseScreen: React.FC<{navigation : any}>  = ({navigation}) => {
+const DelivererChooseScreen: React.FC<{navigation ?: any}> = ({navigation}) => {
   const today: Date = new Date();
   const year: number = today.getFullYear();
   const month: number = today.getMonth() + 1; // MONTHS START AT 0
@@ -13,45 +12,37 @@ const DelivererChooseScreen: React.FC<{navigation : any}>  = ({navigation}) => {
 
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-      </View>
-      <View style={styles.body}>
-        <View style={{ height: 20 }} />
-      <Text style={styles.headerText}>
-          Tournées du {day} {getMonthString(month)} {year}!
+    <View style={{flex: 1, backgroundColor: "#F5F5F5"}}>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Tournées du {day} {getMonthString(month)} {year}
         </Text>
-        <View style={styles.body}>
-          <ToursChoose navigation ={navigation}/>
-        </View>
+        <ToursChoose navigation={navigation} />
       </View>
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#EDBE78",
+    justifyContent: 'center',
+    marginTop: 55,
+    margin: 20,
+    padding: 20,
+    paddingBottom: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 1,
   },
   header: {
-    height: 60,
-    backgroundColor: "#FFFFFF",
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  headerText: {
-    fontSize: 20,
+    fontSize: 19,
     fontWeight: "bold",
-    marginLeft: 10,
-  },
-  body: {
-    flex: 1,
-    backgroundColor: "#EDBE78",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingBottom: 20,
+    alignSelf: "center",
   },
 });
 
